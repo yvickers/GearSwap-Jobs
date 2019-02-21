43227caf9ac4dc7062ea12db645f57424bf5062e
Yancey Vickers <yvickers@gmail.com>
2019-02-21 08:54:10 -0500

    Global gear file
    Should contain gear to be used in multiple jobs - Ambuscade mainly.

---
+++ Mishrahh-Gear.lua
@@ -0,0 +1,17 @@
+globalgear = {}
+
+globalgear.CP = { back="Aptitude Mantle +1" }
+
+globalgear.Meghanada = {}
+globalgear.Meghanada.Head = "Meghanada Visor +2"
+globalgear.Meghanada.Body = "Meg. Cuirie +2"
+globalgear.Meghanada.Hands = "Meg. Gloves +2"
+globalgear.Meghanada.Legs = "Meg. Chausses +2"
+globalgear.Meghanada.Feet = "Meg. Jam. +1"
+
+globalgear.Mummu = {}
+globalgear.Mummu.Head = "Mummu Bonnet +1"
+globalgear.Mummu.Body = "Mummu Jacket +2"
+globalgear.Mummu.Hands = "Mummu Wrists +2"
+globalgear.Mummu.Legs = "Mummu Kecks +1"
+globalgear.Mummu.Feet = "Mummu Gamash. +1"
